0e497f61f665702cdf2a8b060175bfd4f2d28770
FkAX21 <160174513+FkAX21@users.noreply.github.com>
2024-04-05 13:17:41 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+// INTRO TO JAVASCRIPT
+
+// Input Process Output (IPO)
+
+// Output: Alert & Console
+
+// Input: Prompt
+
+// Processing: Data Types
+
+// Processing: Variables
+// var userHeight;
+// let userFaveColour;
+
+// let user_weight = 205;
+// var user_name = "Adonay";
+
+// const capital = "Edmonton";
+
+// userHeight = 6;
+// user_name = "Cloud";
+
+// console.log(userHeight);
+// console.log(user_name);
+
+// String Concatenation & Template Strings
+// let Output1 = user_name + "'s height is " + userHeight + "feet.";
+// console.log(output1);
+
+// let Output2 = `${user_name} lives in ${capital}.`;
+// console.log(Output2);
+// Comments
+
+// Debugging and Testing
+
+// Mad Lib
+
+// Input
+var noun;
+var adjective;
+var verb;
+var number;
+
+noun = prompt("Enter a noun");
+adjective = prompt("Enter an adjective");
+verb = prompt("Enter a verb ");
+number = prompt("Enter a number");
+// Processing
+let Output = `The ${adjective} ${noun} can ${verb} over ${number} kilometers`;
+
+// Output
+alert(Output);
